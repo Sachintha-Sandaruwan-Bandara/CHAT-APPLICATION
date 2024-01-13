05b651ce96805ab853e5e2d3245f04b3f0a70c79
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-01-13 09:51:49 +0530

    content update

---
+++ src/main/java/lk/ijse/CHAT_APPLICATION/controller/ServerFormController.java
@@ -0,0 +1,27 @@
+package lk.ijse.CHAT_APPLICATION.controller;
+/* 
+    @author Sachi_S_Bandara
+    @created 1/13/2024 - 9:48 AM 
+*/
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ServerFormController {
+    @FXML
+    private JFXButton btnSend;
+
+    @FXML
+    private TextArea txtArea;
+
+    @FXML
+    private TextField txtFld;
+
+    @FXML
+    void btnSendOnAction(ActionEvent event) {
+
+    }
+}
